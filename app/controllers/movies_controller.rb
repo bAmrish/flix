2310105abd323a921c894d2a6af243fd38c5bd55
Amrish Bharatiya <bamrish@gmail.com>
2025-03-02 22:59:41 +0530

    Added code for adding a new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,9 +13,23 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title, :rating, :description, :total_gross, :released_on)
     @movie.update(movie_params)
     redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+    redirect_to @movie
+  end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :total_gross, :released_on)
   end
 
 end
