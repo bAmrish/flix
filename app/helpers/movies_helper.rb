d1caac0f78178dc97cba46d683288bb6753aa894
Amrish Bharatiya <bamrish@gmail.com>
2025-03-02 17:38:52 +0530

    Added some view helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+  def total_gross(movie)
+    if movie.flop?
+      "Flop"
+    else
+      number_to_currency(movie.total_gross, precision: 0, unit: "INR ")
+    end
+  end
+
+  def release_date(movie)
+    movie.released_on.strftime("%B %e, %Y")
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+
+  def flop?
+    total_gross <= 10_00_000_00
+  end
 end
