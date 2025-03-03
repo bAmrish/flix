f60e61005b8ef42a316722f70ebdb1f26f3873d3
Amrish Bharatiya <bamrish@gmail.com>
2025-03-03 09:32:33 +0530

    Updated movie index listing to only show released movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ApplicationRecord
 
+  def self.released
+    where("released_on <= ?", Time.now).order("released_on desc")
+  end
+
   def flop?
     total_gross <= 250_000_000
   end
