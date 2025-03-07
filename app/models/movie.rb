44e133947aef7018751ff22d8f39034574461c5f
Amrish Bharatiya <bamrish@gmail.com>
2025-03-07 10:55:11 +0530

    Added validations and error messages

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,18 @@
 class Movie < ApplicationRecord
+  RATINGS = %w"G PG PG-13 R NC-17"
+  validates :title, presence: true
+  validates :released_on, presence: true
+  validates :duration, presence: true
+  validates :description, length: {minimum: 25}
+  validates :total_gross, numericality: {
+    greater_than_or_equal_to: 0
+  }
+  validates :image_file_name, format: {
+    with: /\w+\.(png|jpg)\z/i,
+    message: 'must be a PNG or JPG'
+  } 
+  validates :rating, inclusion: {in: RATINGS}
+
 
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
