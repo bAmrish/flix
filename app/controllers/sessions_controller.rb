6c44ad7378395c212d0fe46d2964018f2a6b10aa
Amrish Bharatiya <bamrish@gmail.com>
2025-03-13 12:10:09 +0530

    Added intial session code with sign up page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  
+  def new    
+  end
+
+  def create
+    username = params[:username]
+    password = params[:password]
+
+    fail
+  end
+
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root "movies#index"
+  root 'movies#index'
 
   resources :movies do 
     resources :reviews
   end
 
+  resource 'session', only: [:new, :create, :delete]
   resources :users
 
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
 end
