2c09f1a96db0bd550a5712438524e64f04a8061a
Amrish Bharatiya <bamrish@gmail.com>
2025-03-13 08:26:44 +0530

    Added profile image for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
+
 module UsersHelper
+  def profile_image(user)
+    tag.img src: user.profile_image, class: 'profile-image'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,38 @@
+require 'digest'
 class User < ApplicationRecord
   has_secure_password
+  BASE_GRAVATAR_IMAGE = 'https://gravatar.com/avatar'
 
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/},
             uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 10, allow_blanks: true}
 
+  class << self
+    attr_accessor :default_image_type
+  end
+
+  def profile_image
+    default_image_type ='name'
+
+    if default_image_type == 'name'
+      profile_image_name
+    else
+      profile_image_monster
+    end
+  end
+
+  def profile_image_monster
+    default_type = 'monsterid'
+    e = email.strip.downcase
+    hash = Digest::SHA256.hexdigest(e)
+    "#{BASE_GRAVATAR_IMAGE}/#{hash}?d=#{default_type}"
+  end
+
+  def profile_image_name
+    default_type = 'initials'
+    e = email.strip.downcase
+    hash = Digest::SHA256.hexdigest(e)
+    "#{BASE_GRAVATAR_IMAGE}/#{hash}?d=#{default_type}&name=#{name}"
+  end
 end
