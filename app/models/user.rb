5809c6501950ac3ae9968fa2e42e6ba3018187a3
Amrish Bharatiya <bamrish@gmail.com>
2025-03-15 18:35:46 +0530

    initial code for favoriting a movie

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   BASE_GRAVATAR_IMAGE = 'https://gravatar.com/avatar'
 
