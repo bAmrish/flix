959749537ab1cd7602629ff49a81e7072fdb2f67
Amrish Bharatiya <bamrish@gmail.com>
2025-03-15 10:50:10 +0530

    Added current user method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if is_logged_in?
+  end
+
+  def is_logged_in?
+    session[:user_id].present?
+  end
 end
