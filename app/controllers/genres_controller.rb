ebcdae608efad29cb3546e0b78e4f3efa8fccbe1
Amrish Bharatiya <bamrish@gmail.com>
2025-03-15 23:45:12 +0530

    Added code to manage genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,26 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to genres_path, status: :see_other, notice: 'Genre added successfully'
+    else
+      @genres = Genre.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to genres_path, status: :see_other, alert: 'Genre deleted successfully'
+  end
+
+private
+  def genre_params
+    params.permit(:name)
+  end
 end
