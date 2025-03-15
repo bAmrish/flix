a1355324beda4676a3d354a9e8968c0a64bd4e7b
Amrish Bharatiya <bamrish@gmail.com>
2025-03-15 13:01:06 +0530

    Added code to peform basic auth check for user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if is_logged_in?
+  end
+
+  def is_logged_in?
+    session[:user_id].present?
+  end
+
+  helper_method :current_user, :is_logged_in?
+
+  def require_signin
+    unless is_logged_in?
+      redirect_to login_path, notice: 'You need to be logged in.'
+    end
+  end
 end
